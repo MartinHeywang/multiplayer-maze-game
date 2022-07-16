6d58c6f206188c79f402235a6637d87745102878
Martin Heywang <martin.heywang1@gmail.com>
2022-07-16 15:49:26 +0200

    client: Set up basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,14 @@ import React, { FC } from "react";
 import HomePage from "./pages/Home";
 
 import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 
 const App: FC<{}> = () => {
     return (
         <div className="App">
-            <HomePage />
+            <Routes>
+                <Route index element={<HomePage />} />
+            </Routes>
         </div>
     );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -8,6 +9,8 @@ import "./assets/scss/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
