94bbbe203f6661f38ee06d0c3b27911e9fe8eda6
Martin Heywang <martin.heywang1@gmail.com>
2022-07-17 18:00:53 +0200

    client: Changed ServerConnexionContext provider methods

--- client/src/contexts/ServerConnectionContext.tsx
+++ client/src/contexts/ServerConnectionContext.tsx
@@ -1,51 +1,91 @@
-import React, { FC, useContext, useState } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 
 import { OurClientSocket } from "otd-types";
 
 import { io } from "socket.io-client";
 
 type ContextValue = {
-    socket: null,
-    connect: (uri: string) => void
-} | {
-    socket: OurClientSocket,
-    disconnect: () => void
+    socket: OurClientSocket | null;
+    connect?: (uri: string) => Promise<OurClientSocket>;
+    disconnect?: () => void;
 };
 
-function errorFn() {
-    throw new Error("ServerConnectionContext is not provided here!");
-}
-
-const ServerConnectionContext = React.createContext<ContextValue>({ socket: null, connect: errorFn });
+// @ts-ignore
+// providing a default value for the state is really useless (because this context is provided everywhere in the app!)
+// and I prefer to put null here as the default value
+// but of course this causes a type error
+const ServerConnectionContext = React.createContext<ContextValue>(null);
 const { Provider, Consumer } = ServerConnectionContext;
 
 const ServerConnectionProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
     const [socket, setSocket] = useState<OurClientSocket | null>(null);
 
+    const isSocketUsable = () => socket !== null && socket.connected;
+
     function connect(uri: string) {
-        const socket = io(uri).connect();
-        setSocket(socket);
+        if (isSocketUsable()) {
+            throw new Error(
+                "Impossible de se connecter, car une autre connexion est active en ce moment."
+            );
+        }
+
+        return new Promise<OurClientSocket>((resolve, reject) => {
+            const socket = io(uri);
+
+            let invalid = false;
+            // function that returns true the first time, then false forever
+            const invalidate = () => {
+                if (invalid) return false;
+                invalid = !invalid;
+                return invalid;
+            };
+
+            // perhaps we will add a timeout here, but we'll keep it simple right now
+
+            socket.once("connect", () => {
+                if (!invalidate()) return;
+
+                resolve(socket);
+            });
+
+            socket.once("connect_error", error => {
+                if (!invalidate()) return;
+
+                reject(error);
+            });
+
+            socket.connect();
+        });
     }
 
     function disconnect() {
         if (!socket) return;
 
-        socket?.disconnect();
+        socket.disconnect();
         setSocket(null);
     }
 
+    useEffect(() => {
+        if (!socket) return;
+
+        const handle = () => setSocket(null);
+
+        socket.on("disconnect", handle);
+
+        return () => {
+            socket.off("disconnect", handle);
+        };
+    }, [socket]);
+
     return (
         <Provider
-            value={(() => {
-                const isSocketUsable = socket !== null && socket.connected;
-                return isSocketUsable? { socket, disconnect } : { socket: null, connect };
-            })()}
+            value={(() => (isSocketUsable() ? { socket, disconnect } : { socket: null, connect }))()}
         >
             {children}
         </Provider>
     );
 };
 
-export const useServerConnection = useContext(ServerConnectionContext);
+export const useServerConnection = () => useContext(ServerConnectionContext);
 
 export { ServerConnectionContext, ServerConnectionProvider, Consumer as ServerConnectionConsumer };
