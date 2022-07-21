81325fcc19bc4d9c8bea33d756c552fcd97fee0c
Martin Heywang <martin.heywang1@gmail.com>
2022-07-21 18:06:04 +0200

    server: Edited game:start event to pass game data

--- server/src/game.ts
+++ server/src/game.ts
@@ -1,36 +1,14 @@
-import { OurServer, OurSocket } from "otd-types";
+import { Cell, Coord, Labyrinth, OurServer, OurSocket } from "otd-types";
 import { Game as GameBase } from "otd-types";
 import { io } from ".";
+import { loadRandomLabyrinth } from "./labyrinths/loader";
 
 import * as player from "./player";
 
-type Game = GameBase & { sockets: Set<OurSocket> };
+type Game = GameBase & { sockets: Set<OurSocket>; labyrinth: Labyrinth };
 
 let scheduledGames: Game[] = [];
 let nextGame: Game | null = null;
-
-function initiateNewGame() {
-    const oldGame = nextGame;
-
-    nextGame = scheduledGames[0] ?? null;
-    if (!nextGame) return;
-
-    scheduledGames.splice(0, 1); // remove the next game from the planned one
-
-    oldGame?.sockets.forEach(socket => {
-        player.editSocketData(socket, { hasJoinedNextGame: false });
-    });
-
-    setTimeout(() => {
-        editGameWithNotify(io, { status: "opened" });
-    }, nextGame.plannedStartTime.getTime() - 1000 * 60 * 5 - Date.now()); // five minutes before the game starts
-
-    setTimeout(() => {
-        editGameWithNotify(io, { status: "playing" });
-
-        io.to("game:next-game").emit("game:start");
-    }, nextGame.plannedStartTime.getTime() - Date.now()); // when the game starts
-}
 
 export function registerSocket(_: OurServer, socket: OurSocket) {
     socket.on("game:join", () => join(socket));
@@ -73,6 +51,60 @@ function join(socket: OurSocket) {
     player.editSocketData(socket, { hasJoinedNextGame: true });
 }
 
+function getCellsAround(coord: Coord) {
+    const labyrinth = nextGame!.labyrinth;
+    const minX = Math.max(coord.x - 2, 0);
+    const minY = Math.max(coord.y - 2, 0);
+    const maxX = Math.min(minX + 4, labyrinth.cells[0].length);
+    const maxY = Math.min(minY + 4, labyrinth.cells.length);
+
+    let cells: Cell[][] = [];
+    let resultX = 0;
+    let resultY = 0;
+    for (let y = minY; y <= maxY; y++) {
+        cells[resultY] = [];
+        for (let x = minX; x <= maxX; x++) {
+            cells[resultY][resultX] = labyrinth.cells[y][x];
+            resultX++;
+        }
+        resultY++;
+    }
+    return cells;
+}
+
+function initiateNewGame() {
+    const oldGame = nextGame;
+
+    nextGame = scheduledGames[0] ?? null;
+    if (!nextGame) return;
+
+    scheduledGames.splice(0, 1); // remove the next game from the planned one
+
+    oldGame?.sockets.forEach(socket => {
+        player.editSocketData(socket, { hasJoinedNextGame: false });
+    });
+
+    const fiveMinutesBeforeGameStart = nextGame.plannedStartTime.getTime() - 1000 * 60 * 5 - Date.now();
+    const atGameStart = nextGame.plannedStartTime.getTime() - Date.now();
+
+    setTimeout(openInscription, fiveMinutesBeforeGameStart);
+    setTimeout(startGame, atGameStart);
+}
+
+function openInscription() {
+    editGameWithNotify(io, { status: "opened" });
+}
+
+function startGame() {
+    editGameWithNotify(io, { status: "playing" });
+
+    io.to("game:next-game").emit(
+        "game:start",
+        getCellsAround(nextGame!.labyrinth.startCoord),
+        nextGame!.labyrinth.startCoord
+    );
+}
+
 // plan all games for the day
 {
     function schedule(hour: number, minute: number) {
@@ -88,6 +120,7 @@ function join(socket: OurSocket) {
             plannedStartTime,
             sockets: new Set(),
             status: "closed",
+            labyrinth: loadRandomLabyrinth(),
         };
 
         scheduledGames.push(game);
--- types/src/socket-io/game.ts
+++ types/src/socket-io/game.ts
@@ -1,4 +1,4 @@
-import { Game } from "../data";
+import { Cell, Coord, Game } from "../data";
 
 export interface CTSEvents {
     "game:watch": (watch: boolean) => void;
@@ -11,7 +11,7 @@ export interface STCEvents {
     "game:update": (game: Game | null) => void;
     "game:error": (msg: string) => void;
 
-    "game:start": () => void;
+    "game:start": (cells: Cell[][], playerPos: Coord) => void;
 }
 
 export type SocketData = {};
