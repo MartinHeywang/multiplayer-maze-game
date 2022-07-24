78cbd234dad404713014da19c06d1adbd6b356d1
Martin Heywang <martin.heywang1@gmail.com>
2022-07-24 19:09:39 +0200

    client: Fixed weird code duplication...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,22 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
 import App from "./App";
+
 import { ServerConnectionProvider } from "./contexts/ServerConnectionContext";
 import { PlayerProvider } from "./contexts/PlayerContext";
 import { GameProvider } from "./contexts/GameContext";
 
 import "normalize.css";
 import "./assets/scss/index.scss";
-import { ServerConnectionProvider } from "./contexts/ServerConnectionContext";
-import { PlayerProvider } from "./contexts/PlayerContext";
-
-const GlobalContexts: FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <ServerConnectionProvider>
-            <PlayerProvider>{children}</PlayerProvider>
-        </ServerConnectionProvider>
-    );
-};
 
 const GlobalContexts: FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
